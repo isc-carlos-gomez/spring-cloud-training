c22e59f19be88c220be6b1aeeef62b5696279ceb
Carlos Gomez <isc.carlos.gomez@gmail.com>
2019-07-09 08:03:50 -0400

    Demo intelligent routing with Spring Gateway & Netflix Zuul

---
+++ goodbye-service/src/main/java/com/krloxz/training/goodbye/GoodbyeServiceApplication.java
@@ -0,0 +1,23 @@
+package com.krloxz.training.goodbye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class GoodbyeServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(GoodbyeServiceApplication.class, args);
+  }
+
+  @RequestMapping
+  public String goodbye() {
+    return "Goodbye!";
+  }
+
+}
