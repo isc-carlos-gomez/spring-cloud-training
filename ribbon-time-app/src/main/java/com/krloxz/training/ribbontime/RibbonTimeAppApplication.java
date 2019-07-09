4e8ff62be58d435c394cc80625f7bcada96c9271
Carlos Gomez <isc.carlos.gomez@gmail.com>
2019-07-09 10:27:26 -0400

    Demo load balancing without a discovery service

--- ribbon-time-app/src/main/java/com/krloxz/training/ribbontime/RibbonTimeAppApplication.java
+++ ribbon-time-app/src/main/java/com/krloxz/training/ribbontime/RibbonTimeAppApplication.java
@@ -3,16 +3,19 @@ package com.krloxz.training.ribbontime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.krloxz.training.config.ribbontime.RibbonTimeConfig;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@RibbonClient(name = "time-service", configuration = RibbonTimeConfig.class)
 @RestController
+@Configuration
 public class RibbonTimeAppApplication {
 
   @Autowired
